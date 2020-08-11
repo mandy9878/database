4b94908d9e374a0f4cb213052b9df40d275035d7
xinyu lin <xinyulin@xinyus-air.myfiosgateway.com>
2020-08-11 17:46:08 -0400

    Update

--- database.js
+++ database.js
@@ -1,13 +1,5 @@
-<!-- The core Firebase JS SDK is always required and must be listed first -->
-<script src="https://www.gstatic.com/firebasejs/7.17.2/firebase-app.js"></script>
-
-<!-- TODO: Add SDKs for Firebase products that you want to use
-     https://firebase.google.com/docs/web/setup#available-libraries -->
-<script src="https://www.gstatic.com/firebasejs/7.17.2/firebase-analytics.js"></script>
-
-<script>
-  // Your web app's Firebase configuration
-  var firebaseConfig = {
+// Your web app's Firebase configuration
+var firebaseConfig = {
     apiKey: "AIzaSyDUKbgK5P8qpFnJsNmSbrwaCLjhzXlycGo",
     authDomain: "my-awesome-project-980708.firebaseapp.com",
     databaseURL: "https://my-awesome-project-980708.firebaseio.com",
@@ -16,8 +8,48 @@
     messagingSenderId: "162946482353",
     appId: "1:162946482353:web:f4ac1186ecd50d9c9ce290",
     measurementId: "G-GTH3E6TY30"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  firebase.analytics();
-</script>
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+let myFlashcards = [ {
+ 
+    "question": "Name this species of bird.",
+ 
+    "image" : "images/barn-owl-2550068__340.webp",
+ 
+    "answer": "Barn Owl"
+ 
+    },
+ 
+    {
+ 
+    "question": "Name this species of bird.",
+ 
+    "image" : "images/dove-2516641__340.webp",
+ 
+    "answer": "Dove"
+ 
+    },
+ 
+    {
+ 
+    "question": "Name this species of bird.",
+ 
+    "image" : "images/hummingbird-2139279__340.webp",
+ 
+    "answer": "Hummingbird"
+ 
+    }
+ 
+  ];
+
+  // Make the database point to the location root -> deck -> flashcards
+// If the location doesn't exist is will be created
+firebase.database().ref('deck/flashcards'); 
+ 
+// myFlashcards will be stored under flashcards in the database
+// Anything that was in this location will be overwritten
+// Thus, a write operation also does an update
+firebase.database().set(myFlashcards);
